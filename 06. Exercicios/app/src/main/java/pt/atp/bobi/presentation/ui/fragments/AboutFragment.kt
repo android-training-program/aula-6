d2f27af8449b4b2f7a189dba578324166a96b90a
Renato Almeida <renato.almeida@talkdesk.com>
2020-11-25 01:02:07 +0000

    Bottom navigation view

---
+++ 06. Exercicios/app/src/main/java/pt/atp/bobi/presentation/ui/fragments/AboutFragment.kt
@@ -0,0 +1,21 @@
+package pt.atp.bobi.presentation.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import pt.atp.bobi.R
+
+class AboutFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+}
---
+++ 06. Exercicios/app/src/main/java/pt/atp/bobi/presentation/ui/fragments/FavouritesFragment.kt
@@ -0,0 +1,20 @@
+package pt.atp.bobi.presentation.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import pt.atp.bobi.R
+
+class FavouritesFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favourites, container, false)
+    }
+}
