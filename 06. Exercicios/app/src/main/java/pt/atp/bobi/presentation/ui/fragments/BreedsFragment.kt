366cff579bd5da6a3478c123d942efa436d79fba
Renato Almeida <renato.almeida@talkdesk.com>
2020-11-26 21:42:33 +0000

    Class content

--- 06. Exercicios/app/src/main/java/pt/atp/bobi/presentation/ui/fragments/BreedsFragment.kt
+++ 06. Exercicios/app/src/main/java/pt/atp/bobi/presentation/ui/fragments/BreedsFragment.kt
@@ -2,6 +2,7 @@ package pt.atp.bobi.presentation.ui.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import pt.atp.bobi.BobiApplication
 import pt.atp.bobi.EXTRA_DOG_BREED
 import pt.atp.bobi.EXTRA_DOG_NAME
 import pt.atp.bobi.R
@@ -20,7 +22,9 @@ private const val TAG = "BreedsFragment"
 
 class BreedsFragment : Fragment() {
 
-    private val viewModel by viewModels<BreedsViewModel>()
+    private val viewModel by viewModels<BreedsViewModel>() {
+        BreedsViewModelFactory((requireActivity().application as BobiApplication).repository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,9 +50,14 @@ class BreedsFragment : Fragment() {
             }
         }
 
-        viewModel.dogsLiveData.observe(viewLifecycleOwner){
-            val adapter = requireView().findViewById<RecyclerView>(R.id.rv_breeds).adapter as BreedsAdapter
+        viewModel.dogsLiveData.observe(viewLifecycleOwner) {
+            val adapter =
+                requireView().findViewById<RecyclerView>(R.id.rv_breeds).adapter as BreedsAdapter
             adapter.submitList(it)
+        }
+
+        viewModel.loadDogsDatabase().observe(viewLifecycleOwner) {
+            Log.d(TAG, "$it")
         }
     }
 
